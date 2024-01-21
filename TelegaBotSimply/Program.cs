865cef21f1a94b0348399a4642fe08702ae45c4f
German Gritsenko <kyperu07@gmail.com>
2024-01-21 21:33:04 +0200

    make simply bot

--- TelegaBotSimply/Program.cs
+++ TelegaBotSimply/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+class Program
+{
+    static void Main()
+    {
+        var BotApiKey = "6952143006:AAGnQWEsAu_hgNyAfIiiK_dPzlPmy6rEn-U";
+
+        TelegramBotClient? _telegramBotClient = new TelegramBotClient(BotApiKey);
+        
+        _telegramBotClient.StartReceiving(UpdateHandler,ErrorHandler);
+        Console.WriteLine("Bot started successfully and listnening the specific channel");
+        Console.ReadKey();
+    }
+
+    public static async Task UpdateHandler(ITelegramBotClient bot, Update update, CancellationToken token)
+    {
+        string responseMessage;
+
+        if (update.Message.Sticker is not null)
+        {
+            responseMessage = "Close your ass";
+        }
+        else
+        {
+            responseMessage = "I coudn't understand you";
+        }
+        
+        await bot.SendTextMessageAsync(update.Message.Chat.Id, responseMessage);
+    }
+
+    private static async Task ErrorHandler(ITelegramBotClient bot, Exception exception, CancellationToken token)
+    {
+          Console.WriteLine("Error Happened");
+    }
+}
